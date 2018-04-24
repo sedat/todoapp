d3747c4e44ff552a09aed3ec18e44e977cbfaf58
Sedat Can Yalçın <sedatcyalcin@gmail.com>
2018-04-24 03:19:43 +0300

    did lots of magic

--- app.js
+++ app.js
@@ -1,17 +1,65 @@
+
 const express = require('express'),
+    mongoose = require('mongoose');
     app = express();
 
+    var bodyParser = require('body-parser');
+
+    app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
+
+mongoose.connect("mongodb://localhost/todo");
+mongoose.Promise = Promise;
+var todoSchema = new mongoose.Schema({
+    todo: String
+});
+
+var Todo = mongoose.model("Todo", todoSchema);
+
 
 app.get('/', (req, res) => {
     res.render('index');
 });
 
 app.get('/do', (req, res) => {
-    res.render('main');
+    Todo.find({}, function(err, alltodos){
+        if(err) {
+            console.log("Error");
+        } else {
+            console.log("render is on");
+            res.render('main', {todo: alltodos});
+        }
+    });
 });
+
+app.post('/do', (req, res) => {
+    console.log(req.body.todo);
+    var postodo = {todo: req.body.todo};
+
+    Todo.create(postodo, function(err, created) {
+        if(err) {
+            console.log(err);
+        } else {
+            res.redirect('/do');
+        }
+    });
+});
+
+app.post('/delete', function(req, res) {
+    Todo.findByIdAndRemove(req.body.id, function(err) {
+        if(err) {
+            console.log("error deleting");
+        } else {
+            res.redirect('/do');
+        }
+    })
+})
+
+app.get('/new', (req, res) => {
+    res.render('new');
+})
 
 
 app.listen(8080, function() {
